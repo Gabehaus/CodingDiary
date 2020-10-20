06e90b44b563d15d0a714660b4e8ccb7098b7dec
Gabehaus <Gabehaus@gmail.com>
2020-10-19 21:42:33 -0700

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+import "./App.css";
+
+import { render } from "react-dom";
+
+import { NavLink } from "reactstrap";
+import { NavLink as RRNavLink } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import Contents from "./Components/Contents.jsx";
+import Clock2 from "./Components/Clock2.jsx";
+import QuoteGen from "./Components/QuoteGen.jsx";
+import DrumMach from "./Components/DrumMach.jsx";
+import Calculator from "./Components/Calculator.jsx";
+import Markdown from "./Components/Markdown.jsx";
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Route exact path="/" component={Contents} />
+      <Route exact path="/clock" component={Clock2} />
+      <Route exact path="/quoteGen" component={QuoteGen} />
+      <Route exact path="/drumMach" component={DrumMach} />
+      <Route exact path="/calculator" component={Calculator} />
+      <Route exact path="/markdownPrev" component={Markdown} />
+    </BrowserRouter>
   );
 }
 
